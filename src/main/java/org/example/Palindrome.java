32be77592799e7189e233a6359308119d12822f3
KripDo <danik2003231@mail.ru>
2023-05-10 11:35:45 +0300

    1 Task

---
+++ src/main/java/org/example/Palindrome.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Palindrome
+{
+
+    public static void main( String[] args ){
+        String word;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите слово.");
+        word = scanner.nextLine();
+        if (check(word)){
+            System.out.println("Слово " + word + " является палиндромом.");
+        }
+        else {
+            System.out.println("Слово " + word + " не является палиндромом.");
+        }
+    }
+
+    public static boolean check(String word){
+        for (int i = 0; i < word.length() / 2 + 1; i++){
+            if(word.toLowerCase().charAt(i) !=  word.toLowerCase().charAt(word.length() - i - 1)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
