778a9454612b213e3b0142aa6c0924211ab0d1cd
KripDo <danik2003231@mail.ru>
2023-05-16 11:24:20 +0300

    Completed Task

---
+++ src/main/java/org/example/Anagram.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Anagram {
+
+    public static void main(String[] args) {
+        examination("bebra", "berar");
+    }
+
+    public static int examination(String firstLine, String secondLine){
+        char[] firstWord = splitWord(firstLine);
+        char[] secondWord = splitWord(secondLine);
+
+        if(checkAnagram(firstWord,secondWord)){
+            System.out.println("Эти слова анограммы.");
+            return 1;
+        }
+        else {
+            System.out.println("Эти слова не являются анограммами");
+            return -1;
+        }
+    }
+
+    public static boolean checkAnagram(char[] firstWord, char[] secondWord){
+        if(firstWord.length != secondWord.length){
+            return false;
+        }
+        Arrays.sort(firstWord);
+        Arrays.sort(secondWord);
+        if(Arrays.equals(firstWord,secondWord)){
+            return true;
+        }
+        return false;
+    }
+
+    public static char[] splitWord(String line){
+        char[] array = new char[line.length()];
+        for (int i =0; i < line.length(); i++){
+            array[i] = line.toLowerCase().charAt(i);
+        }
+        return array;
+    }
+}
---
+++ src/test/java/org/example/AnagramTest.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class AnagramTest extends TestCase {
+    @Test
+    public void testExamination() {
+        String firstWordToTest = "Anagram";
+        String secondWordToTest = "aaNArgm";
+        String thirdWordToTest = "abagram";
+        int firstValueExpected = 1;
+        int secondValueExpected = -1;
+        int thirdValueExpected = -1;
+        assertEquals(firstValueExpected, Anagram.examination(firstWordToTest, secondWordToTest));
+        assertEquals(secondValueExpected, Anagram.examination(firstWordToTest, thirdWordToTest));
+        assertEquals(thirdValueExpected, Anagram.examination(thirdWordToTest, secondWordToTest));
+    }
+}
