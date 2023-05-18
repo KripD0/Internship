dafe9c0a5f9e54c894f2c9ef8f2cfcf59374521f
KripDo <danik2003231@mail.ru>
2023-05-18 11:12:45 +0300

    UniqueSimbol class and Test

---
+++ src/main/java/org/example/UniqueSimbol.java
@@ -0,0 +1,41 @@
+package org.example;
+
+public class UniqueSimbol {
+
+    public static void main(String[] args) {
+        String line = "bbaacfdac";
+        checkCharArray(line);
+
+    }
+
+    public static Character[] splitString(String line){
+        int lineLength = line.length();
+        Character[] array = new Character[lineLength];
+        for (int i = 0; i < lineLength; i++){
+            array[i] = line.charAt(i);
+        }
+        return array;
+    }
+
+    //Сделал для строки, но для массива однотипно там даже не нужно будет разделять.
+    public static Character checkCharArray(String line){
+        Character[] array = splitString(line);
+        boolean flag;
+        int arrayLength = array.length;
+        for (int i = 0; i < arrayLength; i++){
+            flag = false;
+            for (int j = 0; j < arrayLength; j++){
+                if(array[i] == array[j] && i != j){
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag){
+                System.out.println("Первый неповторяющийся символ: " + array[i]);
+                return array[i];
+            }
+        }
+        System.out.println("Не было найдено неповторяющегося символа.");
+        return null;
+    }
+}
---
+++ src/test/java/org/example/UniqueSimbolTest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
+
+class UniqueSimbolTest {
+
+    @Test
+    void checkCharArray() {
+        String firstLine = "aabbccaebcdabdc";
+        String secondLine = "aabbcc";
+
+        Character firstExpected = 'e';
+        Character secondExpected = null;
+
+        assertEquals(firstExpected, UniqueSimbol.checkCharArray(firstLine));
+        assertEquals(secondExpected, UniqueSimbol.checkCharArray(secondLine));
+    }
+}
